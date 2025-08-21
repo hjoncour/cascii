79e1cbbd21f20294024622fce755870689606c7c
Hugo Joncour <hugo.joncour.k2o@gmail.com>
2025-08-20 22:51:32 -0400

    feature/viewer (#4)
    * feature/viewer

---
+++ viewer/src/App.js
@@ -0,0 +1,28 @@
+import './styles/App.css';
+import ASCIIAnimation from './components/ASCIIAnimation';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ASCII Animation Viewer</h1>
+      </header>
+      <main>
+        <h2>Small</h2>
+        <div className="animation-container">
+          <ASCIIAnimation fps={24} frameCount={120} frameFolder="small" className="small-animation" />
+        </div>
+        <h2>Default</h2>
+        <div className="animation-container">
+          <ASCIIAnimation fps={24} frameCount={120} frameFolder="default" className="default-animation" />
+        </div>
+        <h2>Large</h2>
+        <div className="animation-container">
+          <ASCIIAnimation fps={60} frameCount={301} frameFolder="large" className="large-animation" />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
